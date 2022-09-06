dd7922d1509c5538b51e8ebd852237a8065ebddd
jared <112581425+retrieving@users.noreply.github.com>
2022-09-06 22:32:24 +0000

    Working with Data Ch 1

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,16 @@
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15)
+
+rate = input("Current exchange rate for Euro to Dollar: ")
+amount = input("Amount of currency to exchange: ")
+total = int (rate) * int (amount)
+result = total-3
+print(result)

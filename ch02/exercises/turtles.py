71fb67fc6e07f5057dfff72828e9321cea869c76
jared <112581425+retrieving@users.noreply.github.com>
2022-09-07 23:00:25 +0000

    Turtle Square's

---
+++ ch02/exercises/turtles.py
@@ -0,0 +1,50 @@
+import turtle
+
+# window = turtle.Screen()
+
+my_turtle = turtle.Turtle() #Factories
+my_turtle.color("purple")
+length = 50
+my_turtle.forward(length)
+my_turtle.left(90)
+my_turtle.forward(length)
+my_turtle.left(90)
+my_turtle.forward(length)
+my_turtle.left(90)
+my_turtle.forward(length)
+my_turtle.penup()
+
+
+my_turtle.goto(-40, -40)
+
+my_turtle.color("red")
+my_turtle.pendown()
+my_turtle.forward(length)
+my_turtle.left(90)
+my_turtle.forward(length)
+my_turtle.left(90)
+my_turtle.forward(length)
+my_turtle.left(90)
+my_turtle.forward(length)
+my_turtle.left(90)
+my_turtle.forward(length)
+
+# window.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

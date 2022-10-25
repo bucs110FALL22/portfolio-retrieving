527458c21d06278d7d72fed7fbebef4b0748c869
jared <112581425+retrieving@users.noreply.github.com>
2022-10-25 01:45:05 +0000

    ch 6 gui exercise

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,16 @@
+class Player:
+	def player1(you):
+    “””
+				Initialize the player's username               
+               args: pnum [int] the player's username
+		      you.player_num = 1
+          you.lives = 5 
+          you.is_large = True 
+    “””
+  def player2(other)
+    “””
+        Initialize the player's username
+               args: pnum[int] the player's username
+          you.player_num = 2
+          you.lives = 5 
+          you.is_large = False

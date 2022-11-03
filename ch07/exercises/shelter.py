ac2d5f3a3535b20b8cd4a6145fcd7cd6c2923398
jared <112581425+retrieving@users.noreply.github.com>
2022-11-03 03:08:51 +0000

    Shelter exercise chapter 7

---
+++ ch07/exercises/shelter.py
@@ -0,0 +1,29 @@
+import time
+class Animal:
+    def __init__(self, name, type):
+
+        self.id = time.strftime("%d%m%M%S")
+        #self.id = self(id)
+        #self.id = uuid()
+        self.name = name
+        self.type = type
+        self.arrived = time.strftime("%d/%m/%Y")
+        self.adopted = None
+
+    def adoption(self):
+        self.adopted = time.strftime("%d/%m/%Y")
+
+    def __str__(self):
+        result_str = f"{self.name}[{self.type}]"
+        result_str += f"\narrived: {self.arrived}"
+        result_str += f"\nadopted:{self.adopted}"
+        return result_str
+
+def main():
+      Pepsi = Animal("Pepsi", "dog")
+      print(Pepsi)
+      Pepsi.adoption()
+      print(Pepsi)
+
+if __name__ == "__main__": 
+  main()
